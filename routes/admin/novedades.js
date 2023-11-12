bb47aecf662ce0ee596918c36c7a812fe9759b4a
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-11-12 00:14:07 +0000

    arreglo de novedades, programa funcional

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -23,26 +23,33 @@ router.get("/agregar", (req, res, next) => {
 router.post("/agregar", async (req, res, next) => {
   try {
     console.log(req.body);
+
     if (
       req.body.titulo != "" &&
       req.body.subtitulo != "" &&
       req.body.cuerpo != ""
     ) {
+      console.log("condicional ejecutado");
       await novedadesModel.insertNovedad(req.body);
       res.redirect("/admin/novedades");
     } else {
+      var novedades = await novedadesModel.getNovedaddes();
       res.render("admin/novedades", {
         layout: "admin/layout",
+        nombre: req.session.nombre,
+        novedades,
         error: true,
         message: "Todos los campos son requeridos",
       });
     }
   } catch (error) {
+    var novedades = await novedadesModel.getNovedaddes();
     console.log(error);
-    res.render("admin/agregar", {
+    res.render("admin/novedades", {
       layout: "admin/layout",
       error: true,
       message: "No se cargo la novedad",
+      novedades,
     });
   }
 });
