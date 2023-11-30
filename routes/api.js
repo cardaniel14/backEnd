256ec2100124df5208bcaf83a942c1895d992b90
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-11-30 20:50:42 +0000

    creacion e instalacion de nodemailer

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require("nodemailer");
 
 router.get('/novedades', async function (req, res, next) {
 
@@ -27,6 +28,31 @@ router.get('/novedades', async function (req, res, next) {
 		}
 	});
 	res.json(novedades);
-})
+});
+
+router.post('/contacto', async function (req, res, next) {
+	const mail = {
+		to: "carlosderazo.07@gmail.com",
+		subject: "Contacto web",
+		html: `${req.body.name} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email}. <br> Ademas, hizo el siguiente comentario: ${req.body.message} <br> Su telefono es ${req.body.telephone}`,
+	}
+
+	const transport = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASS
+		}
+	});
+
+	await transport.sendMail(mail);
+
+	res.status(201).json({
+		error: false,
+		message: 'Mensaje enviado'
+	})
+	})
+
 
 module.exports = router;
