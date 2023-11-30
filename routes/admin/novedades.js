640352d648611a40e7d823392ea567ea48c00dbf
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-11-30 08:39:51 +0000

    adicionando imagenes

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,33 @@
 var express = require("express");
 var router = express.Router();
 var novedadesModel = require("../../models/novedadesModel");
+var util = require("util");
+var cloudinary = require("cloudinary").v2;
+var uploader = util.promisify(cloudinary.uploader.upload);
 
 router.use(express.static("public"));
 
 router.get("/", async function (req, res, next) {
   var novedades = await novedadesModel.getNovedaddes();
-  console.log("novedades: ", novedades);
+
+  novedades = novedades.map((novedad) => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 80,
+        height: 80,
+        crop: "fill",
+        borderRadius: "20%",
+      });
+      return {
+        ...novedad, //por que le paso el spread?
+        imagen,
+      };
+    } else {
+      return {
+        ...novedad,
+      };
+    }
+  });
   res.render("admin/novedades", {
     layout: "admin/layout",
     nombre: req.session.nombre,
@@ -22,15 +43,23 @@ router.get("/agregar", (req, res, next) => {
 
 router.post("/agregar", async (req, res, next) => {
   try {
-    console.log(req.body);
+    var img_id = "";
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
 
     if (
       req.body.titulo != "" &&
       req.body.subtitulo != "" &&
       req.body.cuerpo != ""
     ) {
-      console.log("condicional ejecutado");
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        //Abre un objeto por que?
+        ...req.body, //Por que tra el spread??
+        img_id,
+      });
       res.redirect("/admin/novedades");
     } else {
       var novedades = await novedadesModel.getNovedaddes();
@@ -81,15 +110,14 @@ router.post("/modificar/", async (req, res, next) => {
   console.log("post/modificar/id");
   try {
     var id = req.body.modifyModalId;
-    console.log("id", id);
+
     var obj = {
       titulo: req.body.modifyModalTitulo,
       subtitulo: req.body.modifyModalSubtitulo,
       cuerpo: req.body.modifyModalCuerpo,
     };
-    console.log("body Id", req.body.modifyModalId);
+
     var novedad1 = await novedadesModel.updateNovedadesById(obj, id);
-    console.log("novedad1", novedad1);
     res.redirect("/admin/novedades");
   } catch (error) {
     console.log(error);
